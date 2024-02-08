edd850f591e200eaae2090c2bdf25217d10c2868
Artur Cullerés Cervera <arturculleres@gmail.com>
2024-02-08 12:05:07 +0100

    changes

--- weather_app/lib/screens/weather_screen.dart
+++ weather_app/lib/screens/weather_screen.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 import 'package:weather_app/services/weather_service.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import '../models/weather_model.dart';
 
 class WeatherScreen extends StatefulWidget {
-  const WeatherScreen ({super.key});
+  const WeatherScreen({super.key});
 
   @override
   State<StatefulWidget> createState() {
@@ -29,26 +31,76 @@ class _WeatherScreenState extends State<WeatherScreen> {
     }
   }
 
+  String getWeatehrAnimation(String? mainCondition) {
+    if (mainCondition == null) return 'assets/weather/sunny.json'; // default
+
+    switch (mainCondition.toLowerCase()) {
+      case 'clouds':
+        return 'assets/weather/cloud.json';
+      case 'fog':
+        return 'assets/weather/cloud.json';
+      case 'thunderstorm':
+        return 'assets/weather/storm.json';
+      case 'shower rain':
+        return 'assets/weather/rainy.json';
+      case 'clear':
+        return 'assets/weather/sunny.json';
+      default:
+        return 'assets/weather/sunny.json';
+    }
+  }
+
   @override
   void initState() {
     super.initState();
     _fetchWeather();
   }
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[800],
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(_weather?.cityName ?? ''),
-            Text('${_weather?.temperature.round()}ºC'),
-
-          ],
+        child: Padding(
+          padding: const EdgeInsets.only(top: 100, bottom: 50),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                _weather?.cityName.toUpperCase() ?? '',
+                style: GoogleFonts.lato(
+                  textStyle: TextStyle(
+                      fontSize: 24,
+                      color: Colors.grey[400],
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+              const Spacer(),
+              Lottie.asset(getWeatehrAnimation(_weather?.mainCondition)),
+              const Spacer(),
+              Text(
+                '${_weather?.temperature.round()}ºC',
+                style: GoogleFonts.lato(
+                  textStyle: TextStyle(
+                      fontSize: 24,
+                      color: Colors.grey[400],
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+              Text(
+                _weather?.mainCondition ?? '',
+                style: GoogleFonts.lato(
+                  textStyle: TextStyle(
+                      fontSize: 24,
+                      color: Colors.grey[400],
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+              const Spacer(),
+            ],
+          ),
         ),
-      )
+      ),
     );
   }
 }
